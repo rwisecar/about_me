85dc952f19abe0fe10739ed723102ff434a6bf26
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-07-19 13:11:47 -0700

    js file updated to inlude new answer one script

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ alert('Nice to meet you, ' + userName + '. Let\'s see how well you know me. I\'m
 
 var answerOne = prompt('Was Rachael born in Seattle?');
 
-if (answerOne = 'Yes' || 'yes' || 'y') {
-consoleLog('Nope, Rachael was born in NYC.')
-} else if (answerOne = 'No' || 'no' || 'n') {
-  consoleLog('That\'s right, she was born in NYC.');
+if (answerOne === 'Yes' || answerOne === 'yes' || answerOne === 'y' || answerOne === 'Y') {
+  alert('Sorry, Rachael was born in NYC.')
+} else if (answerOne === 'No' || answerOne === 'no' || answerOne === 'n' || answerOne === 'N') {
+  alert('That\'s right, she was born in NYC.');
 } else {
-  consoleLog('I\'m sorry, I don\'t understand your answer. Please answer with yes or no.')
+  alert('I\'m sorry, I don\'t understand your answer. Please answer with yes or no.')
 };
