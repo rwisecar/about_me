d7549ea0d29728642f2344fd569c191b9e935701
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-07-19 13:05:51 -0700

    update with first question prompt

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert('Hello')
+var userName = prompt('Hey there! What\'s your name?');
+
+alert('Nice to meet you, ' + userName + '. Let\'s see how well you know me. I\'m going to ask you some questions. Please answer yes or no.');
+
+var answerOne = prompt('Was Rachael born in Seattle?');
+
+if (answerOne = 'Yes' || 'yes' || 'y') {
+consoleLog('Nope, Rachael was born in NYC.')
+} else if (answerOne = 'No' || 'no' || 'n') {
+  consoleLog('That\'s right, she was born in NYC.');
+} else {
+  consoleLog('I\'m sorry, I don\'t understand your answer. Please answer with yes or no.')
+};
